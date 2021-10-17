bbac6601b9d3975533c424f6d6ed954368a0f070
irinafilippenko <irinfilippenko@gmail.com>
2021-10-17 15:46:08 +0300

    Initial commit

---
+++ src/main/java/homework1/HomeWorkApp.java
@@ -0,0 +1,49 @@
+package homework1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    private static void checkSumSign() {
+        int a = 17;
+        int b = -1;
+        if ((a+b) >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    private static void printColor() {
+        int value = 100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void compareNumbers() {
+        int a = 358;
+        int b = 19;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
